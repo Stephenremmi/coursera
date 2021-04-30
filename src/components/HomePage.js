8487cfc6b9b57271edac4cf60e4d264777f25d29
stephen remmi <stephenremmi@gmail.com>
2021-04-30 13:17:56 +0300

    populate index.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+//This file will display our app's home page
+import React from 'react';
+function HomePage() {
+    return (<div className="jumbotron">
+        <h1>Pluralsight Adminstration</h1>
+        <p>React,Flux, and React Router for ultra responsive web apps</p>
+    </div>;
+    )
+}
+export default HomePage
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { render } from 'react-dom';
+import HomePage from './components/HomePage';
+
+//render takes in 2 arguments.1st component that we want to render.2nd argument tells React which argument it should render
+render(<HomePage/>, document.getElementById("root"))
