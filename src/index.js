1051d5277cddcd03c9d0e72cb02621ba4604bff7
stephen remmi <stephenremmi@gmail.com>
2021-04-30 13:42:12 +0300

    Tweak code to AboutPage.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import { render } from 'react-dom';
 import HomePage from './components/HomePage';
-
+import AboutPage from './components/AboutPage';
 //render takes in 2 arguments.1st component that we want to render.2nd argument tells React which argument it should render
-render(<HomePage/>, document.getElementById("root"));
+render(<AboutPage/>, document.getElementById("root"));
