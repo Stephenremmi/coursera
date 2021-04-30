477a258645029a12b90ca911da98f6018c35dc22
stephen remmi <stephenremmi@gmail.com>
2021-04-30 13:27:58 +0300

    correct code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 //This file will display our app's home page
 import React from 'react';
 function HomePage() {
-    return (<div className="jumbotron">
+    return (
+    <div className="jumbotron">
         <h1>Pluralsight Adminstration</h1>
         <p>React,Flux, and React Router for ultra responsive web apps</p>
-    </div>;
+    </div>
     )
 }
 export default HomePage
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { render } from 'react-dom';
 import HomePage from './components/HomePage';
 
 //render takes in 2 arguments.1st component that we want to render.2nd argument tells React which argument it should render
-render(<HomePage/>, document.getElementById("root"))
+render(<HomePage/>, document.getElementById("root"));
